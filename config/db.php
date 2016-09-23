c02652322fe0bab25e1a13e2caf7c48a5679b568
Ivan Filatov <ivan.m.filatov@gmail.com>
2016-09-23 17:46:07 +0300

    removing db config from repo :)

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=t1fortists',
-    'username' => 't1fortists',
-    'password' => 't1Fortists',
-    'charset' => 'utf8',
-];
